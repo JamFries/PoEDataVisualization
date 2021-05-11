9a916d8e5c001d788c647964d7843f2a3b0d6138
jtfra <jtfranz14@gmail.com>
2021-05-11 15:20:59 -0400

    Implemented basic dropdown menu for selecting currency

--- app.py
+++ app.py
@@ -1,44 +1,65 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
+import pandas
 # import plotly.graph_objs as go
 import plotly.express as px
 
+df_currency = pandas.read_csv("Data/Ritual.2021-01-15.2021-04-12.currency.csv", sep=';', parse_dates=['Date'])
+# print(df_currency.dtypes)
+
+# Removes rows so all data is priced in terms of chaos orbs (Ex: 1 alt = 0.3 chaos, not 0.3 chaos = 1 alt)
+df_termsOfChaos = df_currency[df_currency.Get != "Chaos Orb"]
+fig = px.line(df_termsOfChaos, x=df_termsOfChaos["Date"], y=df_termsOfChaos["Value"], color=df_termsOfChaos["Get"],
+              line_group=df_termsOfChaos["Get"], hover_name=df_termsOfChaos["Get"])
+
+# Separate the data into categories to more clearly see things on a graph
+# df_catalysts = df_termsOfChaos[df_termsOfChaos['Get'].str.contains("Catalyst")]
+# fig = px.line(df_catalysts, x=df_catalysts["Date"], y=df_catalysts["Value"], color=df_catalysts["Get"],
+#              line_group=df_catalysts["Get"], hover_name=df_catalysts["Get"])
+
+########################################################################################################################
 app = dash.Dash()
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
+
+app.layout = html.Div([
+    html.H1("PoE Data Visualization using Dash", style={'text-align': 'center'}),
+
+    dcc.Dropdown(
+        id='currency-dropdown',
+        options=[
+            # label is what the user sees. value is the value that matches the data in the dataset
+            {'label': 'Orb of Alteration', 'value': 'Orb of Alteration'},
+            {'label': 'Exalted Orb', 'value': 'Exalted Orb'},
+        ],
+        multi=False,
+        value='Orb of Alteration',
+        style={'width': "40%"},
+        clearable=False,
     ),
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-    dcc.Graph(
-        id='Graph1',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
+    html.Div(id='output-container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='Graph1', figure={})
 ])
+
+# Connect the plotly graph with the dash components
+@app.callback(
+    [Output(component_id='output-container', component_property='children'),
+     Output(component_id='Graph1', component_property='figure')],
+    [Input(component_id='currency-dropdown', component_property='value')]
+)
+def update_graph(optionSelected):
+    print(optionSelected)
+    container = "The currency chosen by the user was: {}".format(optionSelected)
+
+    dfCopy = df_termsOfChaos.copy()
+    dfCopy = df_termsOfChaos[df_termsOfChaos["Get"] == optionSelected] #Set the dataset to be only of the value selected
+
+    fig = px.line(dfCopy, x=dfCopy["Date"], y=dfCopy["Value"])
+
+    return container, fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
