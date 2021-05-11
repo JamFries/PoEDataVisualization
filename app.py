6708c6c638f666a320f2b1424c243330e7b92913
jtfra <jtfranz14@gmail.com>
2021-05-11 17:22:17 -0400

    Implemented different pages to navigate to

--- app.py
+++ app.py
@@ -14,18 +14,23 @@ df_allLeagueCurrency = pandas.concat([df_ritualCurrency, df_heistCurrency])
 
 # Removes rows so all data is priced in terms of chaos orbs (Ex: 1 alt = 0.3 chaos, not 0.3 chaos = 1 alt)
 df_termsOfChaos = df_allLeagueCurrency[df_allLeagueCurrency.Get != "Chaos Orb"]
-# fig = px.line(df_termsOfChaos, x=df_termsOfChaos["Date"], y=df_termsOfChaos["Value"], color=df_termsOfChaos["Get"],
-#               line_group=df_termsOfChaos["Get"], hover_name=df_termsOfChaos["Get"])
 
-# Separate the data into categories to more clearly see things on a graph
-# df_catalysts = df_termsOfChaos[df_termsOfChaos['Get'].str.contains("Catalyst")]
-# fig = px.line(df_catalysts, x=df_catalysts["Date"], y=df_catalysts["Value"], color=df_catalysts["Get"],
-#              line_group=df_catalysts["Get"], hover_name=df_catalysts["Get"])
 
 ########################################################################################################################
 app = dash.Dash()
 
 app.layout = html.Div([
+    dcc.Location(id='url', refresh=False), # represents the URL bar, doesnt render anything
+    html.Div(id='page-content'), # content will be rendered in this element
+])
+
+index_page = html.Div([
+    dcc.Link('Navigate to "/currency"', href='/currency'),
+    html.Br(),
+    dcc.Link('Navigate to "/catalysts"', href='/catalysts'),
+])
+
+currencyLayout = html.Div([
     html.H1("PoE Data Visualization using Dash", style={'text-align': 'center'}),
 
     dcc.Dropdown(
@@ -61,6 +66,59 @@ app.layout = html.Div([
 
     dcc.Graph(id='Graph1', figure={})
 ])
+
+catalystLayout = html.Div([
+    html.H1("PoE Data Visualization using Dash", style={'text-align': 'center'}),
+
+    dcc.Dropdown(
+        id='league-dropdown',
+        options=[
+            {'label': 'Ritual SC', 'value': 'Ritual'},
+            {'label': 'Heist SC', 'value': 'Heist'},
+        ],
+        multi=False,
+        value='Ritual',
+        style={'width': '40%'},
+        clearable=False,
+    ),
+    html.Div(id='league-output-container', children=[]),
+
+    dcc.Dropdown(
+        id='currency-dropdown',
+        options=[
+            # label is what the user sees. value is the value that matches the data in the dataset
+            {'label': 'Fertile Catalyst', 'value': 'Fertile Catalyst'},
+            {'label': 'Intrinsic Catalyst', 'value': 'Intrinsic Catalyst'},
+            {'label': 'Abrasive Catalyst', 'value': 'Abrasive Catalyst'},
+            {'label': 'Tempering Catalyst', 'value': 'Tempering Catalyst'},
+            {'label': 'Turbulent Catalyst', 'value': 'Turbulent Catalyst'},
+            {'label': 'Prismatic Catalyst', 'value': 'Prismatic Catalyst'},
+            {'label': 'Imbued Catalyst', 'value': 'Imbued Catalyst'},
+        ],
+        multi=True,
+        value=['Fertile Catalyst'],
+        style={'width': "40%"},
+    ),
+
+    html.Div(id='currency-output-container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='Graph1', figure={})
+])
+
+@app.callback(
+    Output(component_id='page-content', component_property='children'),
+    [Input(component_id='url', component_property='pathname')]
+)
+def display_page(pathname):
+    if (pathname == '/currency'):
+        return currencyLayout
+    elif (pathname == '/catalysts'):
+        return catalystLayout
+    else:
+        return index_page
+
+
 
 # Connect the plotly graph with the dash components
 @app.callback(
